7149007699cb8af91a5ad3b34def1f1f43eb5b70
breadMSA <69242702+breadMSA@users.noreply.github.com>
2026-07-30 20:52:24 +0800

    Connect site to remote author Worker

--- assets/author-config.js
+++ assets/author-config.js
@@ -1,4 +1,4 @@
 // Leave blank for local visual-editor preview. Do not put a password here.
 // When author-worker is deployed, set its public URL, for example:
 // window.BREAD_AUTHOR_ENDPOINT = "https://bread-author-gate.YOUR.workers.dev";
-window.BREAD_AUTHOR_ENDPOINT = "";
+window.BREAD_AUTHOR_ENDPOINT = "https://bread-author-gate.breadwaseaten.workers.dev";
